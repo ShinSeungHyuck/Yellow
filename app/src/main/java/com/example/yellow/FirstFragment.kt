6f2474d982924524473508bb25b3cf150622c3b4
ShinSeungHyuck <120080284+ShinSeungHyuck@users.noreply.github.com>
2025-09-16 15:17:45 +0900

    9/16 15:10

--- app/src/main/java/com/example/yellow/FirstFragment.kt
+++ app/src/main/java/com/example/yellow/FirstFragment.kt
@@ -1,17 +1,34 @@
 package com.example.yellow
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.yellow.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+    ): View {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        binding.buttonGoToMain.setOnClickListener {
+            val intent = Intent(requireActivity(), MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
