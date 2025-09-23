8710cf785ab094212f5d4dc6353fe266c6a37c88
shinseunghyuck <shinseunghyuck@gmail.com>
2025-09-23 07:59:34 +0000

    0923 1700

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,17 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
+// Java 17 호환성을 위한 Bouncy Castle 라이브러리 버전 강제 지정
+configurations.all {
+    resolutionStrategy {
+        dependencySubstitution {
+            substitute(module("org.bouncycastle:bcprov-jdk18on")).using(module("org.bouncycastle:bcprov-jdk15to18:1.77"))
+            substitute(module("org.bouncycastle:bcutil-jdk18on")).using(module("org.bouncycastle:bcutil-jdk15to18:1.77"))
+            substitute(module("org.bouncycastle:bcpkix-jdk18on")).using(module("org.bouncycastle:bcpkix-jdk15to18:1.77"))
+        }
+    }
+}
+
 android {
     namespace = "com.example.yellow"
     compileSdk = 34
@@ -47,6 +58,9 @@ dependencies {
     // Navigation Component
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
-    implementation(files("libs/TarsosDSPKit-release.aar"))
+
+    // TarsosDSP from JitPack
+    implementation("com.github.JorenSix:TarsosDSP:2.5")
+
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 }
