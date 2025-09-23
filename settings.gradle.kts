c4205e90c9ddca9b15f2f6f4d467eb824ce418c4
shinseunghyuck <shinseunghyuck@gmail.com>
2025-09-23 08:10:31 +0000

    0923 1710

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,12 +14,11 @@ dependencyResolutionManagement {
         maven { url = uri("https://jitpack.io") }
     }
     
-    components {
-        eachComponent {
-            if (details.id.group == "org.bouncycastle" && details.id.name.endsWith("-jdk18on")) {
-                val newName = details.id.name.replace("-jdk18on", "-jdk15to18")
-                details.useTarget(group = details.id.group, name = newName, version = "1.77")
-            }
+    components.all {
+        if (id.group == "org.bouncycastle" && id.name.endsWith("-jdk18on")) {
+            val newName = id.name.replace("-jdk18on", "-jdk15to18")
+            useTarget(group = id.group, name = newName, version = "1.77")
+            because("The jdk18on variant of Bouncy Castle is not compatible with the project's Java 17 environment.")
         }
     }
 }
