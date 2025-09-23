7b817b6d2008a2b142f513b00a5083026e5599ff
shinseunghyuck <shinseunghyuck@gmail.com>
2025-09-23 07:31:40 +0000

    0923 1631

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") apply false
-    id("org.jetbrains.kotlin.android") apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
 
 tasks.register("clean", Delete::class) {
