3041d74efc051d61b21b65d393b13d53bdb57b17
ShinSeungHyuck <120080284+ShinSeungHyuck@users.noreply.github.com>
2025-09-23 14:46:59 +0900

    09231444

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,15 +2,14 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-/*repositories {
+repositories {
     google()
     mavenCentral()
     maven("https://jitpack.io") // <- JitPack 추가
     flatDir {
         dirs("libs") // libs 폴더 참조
     }
-}*/
-
+}
 android {
     namespace = "com.example.yellow"
     compileSdk = 34
@@ -63,8 +62,4 @@ dependencies {
     //implementation("be.tarsos.dsp:jvm:2.5")
     implementation(files("libs/TarsosDSPKit-release.aar"))
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
-    implementation("com.github.leff-software:midi-android:1.0.0")
-    implementation("com.github.billthefarmer:MidiDriver:1.21")
-
-
 }
