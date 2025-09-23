291c16d8117c9cc54fa9219695bac8756bec3ce1
shinseunghyuck <shinseunghyuck@gmail.com>
2025-09-23 08:06:47 +0000

    0923 1706

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,16 @@ dependencyResolutionManagement {
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
     }
+    
+    allConfigurations {
+        resolutionStrategy {
+            dependencySubstitution {
+                substitute(module("org.bouncycastle:bcprov-jdk18on")).using(module("org.bouncycastle:bcprov-jdk15to18:1.77"))
+                substitute(module("org.bouncycastle:bcutil-jdk18on")).using(module("org.bouncycastle:bcutil-jdk15to18:1.77"))
+                substitute(module("org.bouncycastle:bcpkix-jdk18on")).using(module("org.bouncycastle:bcpkix-jdk15to18:1.77"))
+            }
+        }
+    }
 }
 
 rootProject.name = "Yellow"
