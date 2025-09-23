71b5debcd9039c9db174ab670f0342a3148c3a91
shinseunghyuck <shinseunghyuck@gmail.com>
2025-09-23 06:01:01 +0000

    09231500

--- app/src/main/java/com/example/yellow/MainActivity.kt
+++ app/src/main/java/com/example/yellow/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.yellow
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.yellow.databinding.ActivityMainBinding
@@ -17,7 +17,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
